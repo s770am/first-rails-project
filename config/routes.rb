21b51db1ff19e69c8c4fd536c0ee24073997193f
s770am <s770am@gmail.com>
2021-07-20 12:49:05 -0400

    Adding feature to delete pictures

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ post 'pictures' => "pictures#create"
 
 get 'pictures/:id' => "pictures#show", as: "picture"
 
+delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
